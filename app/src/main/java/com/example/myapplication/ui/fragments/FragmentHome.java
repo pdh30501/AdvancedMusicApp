c707de9bf056d45fb4eb40a20965678ba6f0d9c6
Ngoc Huy <vongochuy2802@gmail.com>
2024-05-04 22:41:12 +0700

    test

--- app/src/main/java/com/example/myapplication/ui/fragments/FragmentHome.java
+++ app/src/main/java/com/example/myapplication/ui/fragments/FragmentHome.java
@@ -41,7 +41,7 @@ public class FragmentHome extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        return inflater.inflate(R.layzout.fragment_home,container,false);
     }
 
     @Override
