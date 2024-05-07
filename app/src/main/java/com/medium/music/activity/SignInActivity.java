cf2e550d1f23c49a696077b49cc95ac7d533e7db
khang <vukhaq2910@gmail.com>
2024-05-08 01:24:25 +0700

    SignIn, SignUp

---
+++ app/src/main/java/com/medium/music/activity/SignInActivity.java
@@ -0,0 +1,159 @@
+package com.medium.music.activity;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.medium.music.R;
+import com.medium.music.constant.Constant;
+import com.medium.music.constant.GlobalFunction;
+import com.medium.music.databinding.ActivitySignInBinding;
+import com.medium.music.model.User;
+import com.medium.music.prefs.DataStoreManager;
+import com.medium.music.utils.StringUtil;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignInActivity extends BaseActivity {
+
+    private static final String SERVER_CLIENT_ID = "176952488912-4anh54vvcfls2l5c5lliojgq1is4ija6.apps.googleusercontent.com";
+    private ActivitySignInBinding mActivitySignInBinding;
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+    private static final int REQ_ONE_TAP = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mActivitySignInBinding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(mActivitySignInBinding.getRoot());
+
+        mActivitySignInBinding.rdbUser.setChecked(true);
+
+        mActivitySignInBinding.layoutSignUp.setOnClickListener(
+                v -> GlobalFunction.startActivity(SignInActivity.this, SignUpActivity.class));
+
+        mActivitySignInBinding.btnSignIn.setOnClickListener(v -> onClickValidateSignIn());
+        mActivitySignInBinding.tvForgotPassword.setOnClickListener(v -> onClickForgotPassword());
+
+
+        // Google Sign In one tap
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        .setServerClientId(SERVER_CLIENT_ID)
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                .build();
+
+        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher =
+                registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), result -> {
+                    try {
+                        SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
+                        String idToken = credential.getGoogleIdToken();
+                        if (idToken != null) {
+                            String email = credential.getId();
+                            String username = credential.getDisplayName();
+
+                            String password = "0";
+                            User userObject = new User(email, password);
+                            if (email != null && email.contains(Constant.ADMIN_EMAIL_FORMAT)) {
+                                userObject.setAdmin(true);
+                            }
+                            DataStoreManager.setUser(userObject);
+                            GlobalFunction.startActivity(SignInActivity.this, MainActivity.class);
+                            finishAffinity();
+                        }
+                    } catch (ApiException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        mActivitySignInBinding.btnSinginGoogle.setOnClickListener(view -> oneTapClient.beginSignIn(signInRequest)
+                .addOnSuccessListener(this, result -> {
+                    IntentSenderRequest intentSenderRequest =
+                            new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
+                    activityResultLauncher.launch(intentSenderRequest);
+                })
+                .addOnFailureListener(this, e -> Log.d(TAG, e.getLocalizedMessage())));
+    }
+
+
+    private void onClickForgotPassword() {
+        GlobalFunction.startActivity(this, ForgotPasswordActivity.class);
+    }
+
+    private void onClickValidateSignIn() {
+        String strEmail = mActivitySignInBinding.edtEmail.getText().toString().trim();
+        String strPassword = mActivitySignInBinding.edtPassword.getText().toString().trim();
+        if (StringUtil.isEmpty(strEmail)) {
+            Toast.makeText(SignInActivity.this, getString(R.string.msg_email_require), Toast.LENGTH_SHORT).show();
+        } else if (StringUtil.isEmpty(strPassword)) {
+            Toast.makeText(SignInActivity.this, getString(R.string.msg_password_require), Toast.LENGTH_SHORT).show();
+        } else if (!StringUtil.isValidEmail(strEmail)) {
+            Toast.makeText(SignInActivity.this, getString(R.string.msg_email_invalid), Toast.LENGTH_SHORT).show();
+        } else {
+            if (mActivitySignInBinding.rdbAdmin.isChecked()) {
+                if (!strEmail.contains(Constant.ADMIN_EMAIL_FORMAT)) {
+                    Toast.makeText(SignInActivity.this, getString(R.string.msg_email_invalid_admin), Toast.LENGTH_SHORT).show();
+                } else {
+                    signInUser(strEmail, strPassword);
+                }
+                return;
+            }
+
+            if (strEmail.contains(Constant.ADMIN_EMAIL_FORMAT)) {
+                Toast.makeText(SignInActivity.this, getString(R.string.msg_email_invalid_user), Toast.LENGTH_SHORT).show();
+            } else {
+                signInUser(strEmail, strPassword);
+            }
+        }
+    }
+
+    private void signInUser(String email, String password) {
+        showProgressDialog(true);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    showProgressDialog(false);
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        if (user != null) {
+                            User userObject = new User(user.getEmail(), password);
+                            if (user.getEmail() != null && user.getEmail().contains(Constant.ADMIN_EMAIL_FORMAT)) {
+                                userObject.setAdmin(true);
+                            }
+                            DataStoreManager.setUser(userObject);
+                            GlobalFunction.startActivity(SignInActivity.this, MainActivity.class);
+                            finishAffinity();
+                        }
+                    } else {
+                        Toast.makeText(SignInActivity.this, getString(R.string.msg_sign_in_error),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
